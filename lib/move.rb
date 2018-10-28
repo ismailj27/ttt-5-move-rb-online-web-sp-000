c3c1278cf39699c6834a6dea27d219d98c1b78ae
Ismail Jalloh <ijalloh21@gmail.com>
2018-10-28 01:44:43 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,18 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+def display_board(new_board)
+  puts " #{new_board[0]} | #{new_board[1]} | #{new_board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{new_board[3]} | #{new_board[4]} | #{new_board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{new_board[6]} | #{new_board[7]} | #{new_board[8]} "
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(position)
+  position = position.to_i
+  index = position - 1
+end
+
+def move(new_board, index, default_player = "X")
+  new_board[index] = default_player
+end
